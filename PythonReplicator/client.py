0229716e87c76a48c8ba725de70f0f941b3452a4
Joonamo <joonamo@gmail.com>
2018-03-25 16:45:07 +0000

    Improved client

--- PythonReplicator/client.py
+++ PythonReplicator/client.py
@@ -1,22 +1,26 @@
-
+import traceback
 import sys
 import serial
 import asyncio
 import websockets
+import time
 
-ser = serial.Serial("dev/ttyAMA0", 115200)
+ser = serial.Serial("/dev/ttyAMA0", 115200)
 
 async def client():
     async with websockets.connect('ws://%s:3939'% sys.argv[1]) as websocket:
+        print("Connected!")
         while True:
             message = await websocket.recv()
             print(message)
-            ser.write(bytes("b" + message, "utf-8"))
+            ser.write(bytes("d" + message, "utf-8"))
 
 while True:
     try:
+        time.sleep(1)
+        print("Connecting...")
         asyncio.get_event_loop().run_until_complete(client())
     except KeyboardInterrupt:
         break
     except:
-        pass
+        traceback.print_exc()
